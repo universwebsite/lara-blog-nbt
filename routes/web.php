8dfd48ba7b98705b868c4d9e05e5426b6be053f5
uniweb <uniweb@inbox.ru>
2019-02-25 19:52:33 +0500

    администрирование nbt-иправлен index.blade

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['prefix'=>'admin', 'namespace'=>'Admin', 'middleware'=>['auth']], function(){
+    Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+    Route::resource('/category', 'CategoryController', ['as'=>'admin']);
+});
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +22,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/about', 'AboutController@index')->name('about');
