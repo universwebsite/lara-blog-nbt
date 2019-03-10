6c627ff1385e0b92718a35b533aa949f7a9c1d90
uniweb <uniweb@inbox.ru>
2019-03-10 23:01:34 +0500

    13 панель состояния

---
+++ app/Providers/BlogServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+
+class BlogServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->topMenu();
+    }
+    //
+    public function topMenu() {
+        View::composer('layouts.header', function ($view) {
+            $view->with('categories', \App\Category::where('parent_id', 0)->where('published', 1)->get());
+        });
+    }
+}
---
+++ resources/views/layouts/top_menu.blade.php
@@ -0,0 +1,11 @@
+@foreach ($categories as $category)
+
+    @if ($category->children->where('published')->count())
+    <li class="dropdown">
+        <a href="{{url("/blog/category/$category->slug")}}" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+            {{$category->title}} <span class="caret"></span>            
+        </a>
+        <ul class="dropdown-menu" role="menu">
+            @include('layouts.top_menu', ['categories' => $category->children])
+        </ul>
+    </li>
